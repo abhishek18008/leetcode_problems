e0257a80b81e3f76b0a618b3a3b839c25a04ae58
Abhishek Kumar <abhishekkumargupta18008@gmail.com>
2023-10-20 22:31:35 +0530

    solution added for water flow problem

--- 417.pacific-atlantic-water-flow.cpp
+++ 417.pacific-atlantic-water-flow.cpp
@@ -1,28 +1,86 @@
-/*
- * @lc app=leetcode id=417 lang=cpp
- *
- * [417] Pacific Atlantic Water Flow
- */
-
-// @lc code=start
 class Solution {
+    typedef pair<int, int> P;
 public:
-
     vector<vector<int>> pacific;
-    vector<vector<int>> Atlantic;
+    vector<vector<int>> atlantic;
 
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int row=heights.size();
-        int col=heights[0].size();
+        int row = heights.size();
+        int col = heights[0].size();
 
-        pacific.resize(row,vector<int>(col,0));
-        Atlantic.resize(row,vector<int>(col,0));
+        pacific.resize(row, vector<int>(col, 0));
+        atlantic.resize(row, vector<int>(col, 0));
 
-        //use bfs to find all points from where water can flow
+        queue<P> q;
 
-        //left edge
-        for(int i=0;i<)
+        // Left edge
+        for (int i = 0; i < row; i++) {
+            q.push({i, 0});
+            pacific[i][0] = 1;
+        }
+
+        // Top edge
+        for (int i = 1; i < col; i++) {
+            q.push({0, i});
+            pacific[0][i] = 1;
+        }
+
+        while (!q.empty()) {
+            auto idx = q.front();
+            q.pop();
+
+            int dx[4] = {0, 1, -1, 0};
+            int dy[4] = {1, 0, 0, -1};
+            for (int i = 0; i < 4; i++) {
+                int newx = idx.first + dx[i];
+                int newy = idx.second + dy[i];
+                if (newx >= 0 && newx < row && newy >= 0 && newy < col 
+                    && heights[newx][newy] >= heights[idx.first][idx.second]
+                    && !pacific[newx][newy]) {
+                    q.push({newx, newy});
+                    pacific[newx][newy] = 1;
+                }
+            }
+        }
+
+        // Right edge
+        for (int i = 0; i < row; i++) {
+            q.push({i, col - 1});
+            atlantic[i][col - 1] = 1;
+        }
+
+        // Bottom edge
+        for (int i = 0; i < col; i++) {
+            q.push({row - 1, i});
+            atlantic[row - 1][i] = 1;
+        }
+
+        while (!q.empty()) {
+            auto idx = q.front();
+            q.pop();
+            int dx[4] = {0, 1, -1, 0};
+            int dy[4] = {1, 0, 0, -1};
+            for (int i = 0; i < 4; i++) {
+                int newx = idx.first + dx[i];
+                int newy = idx.second + dy[i];
+                if (newx >= 0 && newx < row && newy >= 0 && newy < col 
+                    && heights[newx][newy] >= heights[idx.first][idx.second]
+                    && !atlantic[newx][newy]) {
+                    q.push({newx, newy});
+                    atlantic[newx][newy] = 1;
+                }
+            }
+        }
+
+        vector<vector<int>> ans;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (atlantic[i][j] && pacific[i][j]) {
+                    ans.push_back({i, j});
+                }
+            }
+        }
+
+        return ans;
     }
 };
-// @lc code=end
-
